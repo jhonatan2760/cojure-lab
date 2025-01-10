71cf8f06510596a170caf89fc1db98a3d3f906c8
Jhonatan S. Souza <jhonatan2760@gmail.com>
2025-01-09 22:37:16 -0300

    Create new project structure

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject clojure-lab "0.1.0-SNAPSHOT"
-  :description "A simple Clojure project"
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [prismatic/schema "1.1.12"]]
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot clojure-lab.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
