5fd9fb842d933c258ea6ec3ee8b3d6cf7efbae83
Jhonatan S. Souza <jhonatan2760@gmail.com>
2025-02-11 13:49:10 -0300

    Work in progress

--- src/clojure_lab/core.clj
+++ src/clojure_lab/core.clj
@@ -7,7 +7,15 @@
    :fuel-level 100
    :turbo-pressure 0.0})
 
+(def fastback-abarth
+  {:model "Fastback Abarth"
+   :engine "GSE-T270"
+   :fuel-level 99.3
+   :turbo-pressure 0.5})
+
 (defn -main
   "Execute car routine..."
   [& args]
-  (println (car/insert-more-gas! pulse-abarth 120)))
+  (-> pulse-abarth
+      (car/insert-more-gas! 120)
+      (println)))
